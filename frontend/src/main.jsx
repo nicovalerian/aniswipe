b00287221675e634b71a5a79d6d529f462c1ab40
Nico <nicovalerian2@gmail.com>
2025-05-02 17:49:50 +0700

    Set up proper Chakra UI dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css' // Keep your global CSS if needed
+import { ChakraProvider } from '@chakra-ui/react'
 import { UserProvider } from './context/UserContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <ChakraProvider /*theme={theme}*/ >
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </ChakraProvider>
   </React.StrictMode>,
 )
